1068d6b0cf530e68bd2fdd56bd1fb5679cebd18b
vivian254338489 <owen254338@gmail.com>
2026-05-06 23:28:24 +0800

    Add START.bat

---
+++ START.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+cd /d "%~dp0"
+echo Installing dependencies...
+call npm install
+if errorlevel 1 goto failed
+echo Checking local environment...
+call npm run preflight
+if errorlevel 1 goto failed
+echo Starting TKEN Claude Web UI...
+echo Open http://localhost:8789
+start "" /b node open-browser.mjs http://localhost:8789 2500
+call npm start
+goto end
+
+:failed
+echo Setup failed. Make sure Node.js 20+ is installed.
+pause
+
+:end
